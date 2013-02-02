438eec9c472686b9873c753c644c2eaca29540a1
ootsubo <hirokin0625@gmail.com>
2013-02-03 02:12:41 +0900

    mod

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/Users/ootsubo/work/php/install/phpunit-all-in-one/vendor/autoload.php';
+require_once '../install/phpunit-all-in-one/vendor/autoload.php';
 
 spl_autoload_register(function ($class) {
         require_once __DIR__ . "/class/$class.php";
